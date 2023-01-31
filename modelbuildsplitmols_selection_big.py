bba12634c5aeb944dc89c06d179761dfd6615c57
Loriano Storchi <loriano@storchi.org>
2023-01-31 16:35:16 +0100

    add swapper to read subsets one after the other

--- modelbuildsplitmols_selection_big.py
+++ modelbuildsplitmols_selection_big.py
@@ -28,11 +28,40 @@ import models
 #import visualkeras
 #from PIL import ImageFont
 
+#####################################################################################
+
+class readergenerator(tf.keras.utils.Sequence):
+
+  def __init__(self, filenames, labels, batch_size) :
+    self.filenames = filenames
+    self.labels = labels
+    self.batch_size = batch_size
+
+    self.dimx = 0
+    self.dimy = 0
+    self.dimz = 0
+    
+  def __len__(self) :
+    return (np.ceil(len(self.filenames) / float(self.batch_size))).astype(int)
+  
+  def __getitem__(self, idx) :
+    batch_x = self.filenames[idx * self.batch_size : (idx+1) * self.batch_size]
+    batch_y = self.labels[idx * self.batch_size : (idx+1) * self.batch_size]
+
+    cn = 8
+    X = []    
+    for file_name in batch_x:
+        treedobject, self.dimx, self.dimy, self.dimz = \
+            commonutils.readfeature("", file_name, cn)
+        X.append(treedobject)
+        #print(file_name)
+
+    return np.array(X), np.array(batch_y)
+
 #####################################################################################3
 
 if __name__ == "__main__":
 
-    path = "./dbs/BBB_full/"
     cn = 8
     inunits = 64
     indense_layers = 4
@@ -44,9 +73,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--input-path", help="Specify the path where files are placed: " +  path , \
-        type=str, required=False, default=path, dest="inputpath")
-    parser.add_argument("--labelfile", help="Read labels file", \
+    parser.add_argument("--trainfilenames", help="Specify the input training filenames ", 
+        type=str, required=True)
+    parser.add_argument("--trainlabels", help="Read train labels file", \
+        type=str, required=True)
+    parser.add_argument("--validfilenames", help="Specify the input training filenames ", 
+        type=str, required=True)
+    parser.add_argument("--validlabels", help="Read train labels file", \
         type=str, required=True)
 
     parser.add_argument("--nunits", \
@@ -64,8 +97,6 @@ if __name__ == "__main__":
         type=str, required=False, default=modelname)
     parser.add_argument("--channels", help="Specify channels to be used, default: " + str(cn)  , \
         type=int, required=False, default=cn)
-    parser.add_argument("--splitter", help="Specify npz filename splitter, default: " + npzsplit , \
-        type=str, required=False, default=npzsplit)
     parser.add_argument("--dumppredictions", help="Dump predicted data in CSV files", \
         action='store_true', default=False)
 
@@ -73,232 +104,51 @@ if __name__ == "__main__":
 
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-    path = args.inputpath
     cn = args.channels
     inunits = args.nunits
     indense_layers = args.ndenselayers
     nepochs = args.nepochs
     nbatch_size = args.nbatchsize
     modelname = args.modelname
-    npzsplit = args.splitter
 
     cnformodel = cn
 
-    labels = {}
-    random.seed(1)
+    batch_size = 500
 
-    fp = open(args.labelfile, "r")
+    X_train_filenames = np.load(args.trainfilenames)
+    X_val_filenames = np.load(args.validfilenames)
+    y_train = np.load(args.trainlabels)
+    y_val = np.load(args.validlabels)
 
-    listnames = []
+    # read firts fopr dimension
+    dimx = dimy = dimz = 0
+    name = X_train_filenames[0]
+    treedobject, dimx, dimy, dimz = commonutils.readfeature("", name, cn)
+    print("Reading first element dimensions: ", dimx, dimy, dimz)
+ 
+    training_batch_generator = readergenerator(X_train_filenames, y_train, batch_size)
+    validation_batch_generator = readergenerator(X_val_filenames, y_val, batch_size)
 
-    for line in fp:
-        sline = line.split()
-
-        if len(sline) != 2:
-            print("Error in line ", line, file=sys.stderr)
-        else:
-            if (commonutils.check_float(sline[1])):
-                val = int(sline[1])
-                
-                if (sline[0] in labels):
-                    print("Error filename ", sline[0], " already in")
-                    print(line)
-
-                labels[sline[0]] = val
-                listnames.append(sline[0])
-            else:
-                print("Error in line ", line, "non numeric value ", file=sys.stderr)
-
-    if len(listnames) != len(labels):
-        print("duplicated file")
-        exit(1)
-
-    trainvalidsetdim = 0.80
-    validsetdim = 0.20
-    testsetdim = 0.20
-
-    trainvalid_size = int(trainvalidsetdim * len(labels))
-    fullsize = len(labels)
-    train_keys = random.sample(list(labels), trainvalid_size)
-
-    trainvalid_labels = {}
-    test_labels = {}
-    for k in labels:
-        if k in train_keys:
-            trainvalid_labels[k] = labels[k]
-        else:
-            test_labels[k] = labels[k]
-
-    test_size = int(testsetdim * len(test_labels))
-    test_keys = random.sample(list(test_labels), test_size)
-
-    print("Training+Validation Molecules: ", len(trainvalid_labels), flush=True)
-    print("Test Molecules: ", len(test_labels), flush=True)
-
-    if ((len(trainvalid_labels) + len(test_labels)) != len(labels)):
-        print("Error in diemsnion of sets")
-        exit(1)
-
-    # in case to check set
-    # print(list(train_labels.items())[1])
-    # print(list(train_labels.items())[2])
-    # print(list(val_labels.items())[3])
-    # print(list(val_labels.items())[6])
-
-    counter = 1;
-    for i, j in trainvalid_labels.items():
-        print("InitialTrain+Validset ", counter, i, j, flush=True)
-        counter += 1
-
-    for i, j in test_labels.items():
-        print("InitialTestset  ", counter, i, j, flush=True)
-        counter += 1
-
-    ext = ".npz" # devo provare a  caricarli tutti 
-    
-    train_names = []
-    val_names = []
-    test_names = []
-
-    first = True
-
-    glob_dimx = 0
-    glob_dimy = 0
-    glob_dimy = 0
-
-    cnformodel = cn
-    for idx, files in enumerate(os.listdir(path)):
-        if files.endswith(ext):
-
-            name = files.split(".")[0]
-
-            if first:
-                treedobject, glob_dimx, glob_dimy, glob_dimz = \
-                    commonutils.readfeature (path, files, cn)
-                first = False
-
-            #basename = name.split(npzsplit)[0]
-            basename = re.split(npzsplit, name)[0]
-            #print("Reading: ", name," and match ", basename)
-            #print("  Dimensions: ",dimx, dimy, dimz)
-            
-            if basename in trainvalid_labels:
-                rnd = random.uniform(0.0,1.0)
-
-                if rnd > validsetdim:
-                    train_names.append(name)
-                else:
-                    val_names.append(name)
-            elif basename in test_labels:
-                test_names.append(name)
-            else:
-                print("Reading: ", name," and match ", basename)
-                print("  Dimensions: ",dimx, dimy, dimz)
-                print("    Not in Set")
-                sys.stdout.flush()
-
-    print("Start reading files...", flush=True)
-
-    cnformodel = cn
-
-    yl_train = []
-    yl_val = []
-    yl_test = []
-
-    X_test = np.zeros((len(test_names), glob_dimx, glob_dimy, glob_dimz, cnformodel),\
-         dtype=np.float32)
-    X_train = np.zeros((len(train_names), glob_dimx, glob_dimy, glob_dimz, cnformodel), \
-        dtype=np.float32)
-    X_val = np.zeros((len(val_names), glob_dimx, glob_dimy, glob_dimz, cnformodel), \
-        dtype=np.float32)
-
-    trainidx = 0
-    testidx = 0
-    validx = 0
-
-    for idx, files in enumerate(os.listdir(path)):
-        if files.endswith(ext):
-
-            name = files.split(".")[0]
-
-            treedobject, dimx, dimy, dimz = \
-                commonutils.readfeature (path, files, cn)
-
-            if dimx != None:
-                if dimx != glob_dimx or \
-                    dimy != glob_dimy or \
-                        dimz != glob_dimz:
-                    print(files, file= sys.stderr)
-                    print("Dimension problem was ", glob_dimx, glob_dimy, glob_dimz, \
-                        file=sys.stderr)
-                    print("                  now ", dimx, dimy, dimz, \
-                        file=sys.stderr)
-                    exit(1)
-                else:
-                    #basename = name.split(npzsplit)[0]
-                    basename = re.split(npzsplit, name)[0]
-                    #print("Reading: ", name," and match ", basename)
-                    #print("  Dimensions: ",dimx, dimy, dimz)
-                    
-                    if name in train_names:
-                        yl_train.append(labels[basename])
-                        X_train[trainidx, :,  :, :, :] =  treedobject
-                        trainidx += 1
-                    elif name in val_names:
-                        yl_val.append(labels[basename])
-                        X_val[validx, :,  :, :, :] =  treedobject
-                        validx *= 1
-                    elif name in test_names:
-                        yl_test.append(labels[basename])
-                        X_test[testidx, :,  :, :, :] =  treedobject
-                        testidx += 1
-                    else:
-                        print("Reading: ", name," and match ", basename)
-                        print("  Dimensions: ",dimx, dimy, dimz)
-                        print("    Not in Set")
-                        sys.stdout.flush()
-            else:
-                print("    Error in reading", name)
-                exit(1)
-
-    print("Done...", flush=True)
-
-    print("Train: ", X_train.shape, len(yl_train))
-    print("Val:   ", X_val.shape, len(yl_val))
-    print("Test:  ", X_test.shape, len(yl_test))
-
-    labels_train = to_categorical(yl_train)
-    labels_val = to_categorical(yl_val)
-    labels_test = to_categorical(yl_test)
-
-    #for n in train_mol_to_idxvp:
-    #    print(n , " ==> ",train_mol_to_idxvp[n])
-    #    for i in train_mol_to_idxvp[n]:
-    #        print("    ", y_train[i])
-
-    sample_shape = (glob_dimx, glob_dimy, glob_dimz, cnformodel)
+    sample_shape = (dimx, dimy, dimz, cnformodel)
 
     print("Sample shape: ", sample_shape)
    
-    #model = models.model_scirep_selection(sample_shape, indense_layers, inunits, infilters)
-
     model = models.model_scirep_selection_hyperopt(sample_shape, indense_layers, inunits, \
         [32,32,32], (3,3,3), (2, 2, 2), True)
 
-    #font = ImageFont.truetype("arial.ttf", 12)
-    #visualkeras.layered_view(model, legend=True, font=font, draw_volume=False) 
-
-    # lower learning rate the mse is stucked
     K.set_value(model.optimizer.learning_rate, 0.0001)
     print("Learning rate before second fit:", model.optimizer.learning_rate.numpy())
     
     model.summary()
-    history = model.fit(X_train, labels_train,
-                        batch_size=nbatch_size,
-                        epochs=nepochs,
-                        verbose=1,
-                        validation_data=(X_val, labels_val))
-    
+
+    history = model.fit(training_batch_generator,
+                   steps_per_epoch = int(3800 // batch_size),
+                   epochs = 10,
+                   verbose = 1,
+                   validation_data = validation_batch_generator,
+                   validation_steps = int(950 // batch_size))
+
+
     print("")
     print ("Epoch Loss ValLoss")
     for i in range(len(history.history['loss'])):
---
+++ modelbuildsplitmols_selection_big_prepare.py
@@ -0,0 +1,150 @@
+import os
+import re
+import sys
+import random
+import argparse
+import numpy as np
+
+from tensorflow.keras.utils import to_categorical
+
+import sys
+sys.path.append("./common")
+
+import commonutils
+
+#####################################################################################3
+
+if __name__ == "__main__":
+
+    path = "./dbs/BBB_full/"
+    npzsplit = "_c0_"
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input-path", help="Specify the path where files are placed: " +  path , \
+        type=str, required=False, default=path, dest="inputpath")
+    parser.add_argument("--labelfile", help="Read labels file", \
+        type=str, required=True)
+    parser.add_argument("--splitter", help="Specify npz filename splitter, default: " + npzsplit , \
+        type=str, required=False, default=npzsplit)
+
+    args = parser.parse_args()
+
+    path = args.inputpath
+    npzsplit = args.splitter
+
+    labels = {}
+    random.seed(1)
+
+    fp = open(args.labelfile, "r")
+
+    listnames = []
+
+    for line in fp:
+        sline = line.split()
+
+        if len(sline) != 2:
+            print("Error in line ", line, file=sys.stderr)
+        else:
+            if (commonutils.check_float(sline[1])):
+                val = int(sline[1])
+                
+                if (sline[0] in labels):
+                    print("Error filename ", sline[0], " already in")
+                    print(line)
+
+                labels[sline[0]] = val
+                listnames.append(sline[0])
+            else:
+                print("Error in line ", line, "non numeric value ", file=sys.stderr)
+                exit(1)
+
+    if len(listnames) != len(labels):
+        print("duplicated file")
+        exit(1)
+
+    trainvalidsetdim = 0.80
+    validsetdim = 0.20
+    testsetdim = 0.20
+
+    trainvalid_size = int(trainvalidsetdim * len(labels))
+    fullsize = len(labels)
+    train_keys = random.sample(list(labels), trainvalid_size)
+
+    trainvalid_labels = {}
+    test_labels = {}
+    for k in labels:
+        if k in train_keys:
+            trainvalid_labels[k] = labels[k]
+        else:
+            test_labels[k] = labels[k]
+
+    test_size = int(testsetdim * len(test_labels))
+    test_keys = random.sample(list(test_labels), test_size)
+
+    print("Training+Validation Molecules: ", len(trainvalid_labels), flush=True)
+    print("Test Molecules: ", len(test_labels), flush=True)
+
+    if ((len(trainvalid_labels) + len(test_labels)) != len(labels)):
+        print("Error in diemsnion of sets")
+        exit(1)
+
+    counter = 1;
+    for i, j in trainvalid_labels.items():
+        print("InitialTrain+Validset ", counter, i, j, flush=True)
+        counter += 1
+
+    for i, j in test_labels.items():
+        print("InitialTestset  ", counter, i, j, flush=True)
+        counter += 1
+
+    ext = ".npz" # devo provare a  caricarli tutti 
+    
+    train_names = []
+    val_names = []
+    test_names = []
+
+    yl_train = []
+    yl_val = []
+    yl_test = []
+
+    for idx, files in enumerate(os.listdir(path)):
+        if files.endswith(ext):
+
+            name = files.split(".")[0]
+            basename = re.split(npzsplit, name)[0]
+            
+            if basename in trainvalid_labels:
+                rnd = random.uniform(0.0,1.0)
+
+                if rnd > validsetdim:
+                    train_names.append(path+"/"+files)
+                    yl_train.append(labels[basename])
+                else:
+                    val_names.append(path+"/"+files)
+                    yl_val.append(labels[basename])
+            elif basename in test_labels:
+                test_names.append(path+"/"+files)
+                yl_test.append(labels[basename])
+            else:
+                print(name," and match ", basename)
+                print("    Not in Set")
+                sys.stdout.flush()
+
+    print("Done...", flush=True)
+
+    labels_train = to_categorical(yl_train)
+    labels_val = to_categorical(yl_val)
+    labels_test = to_categorical(yl_test)
+
+    print("Training: ", len(train_names), labels_train.shape)
+    np.save("training_filenames.npy", train_names)
+    np.save("training_labels.npy", labels_train)
+
+    print("Validation: ", len(val_names), labels_val.shape)
+    np.save("validation_filenames.npy", val_names)
+    np.save("validation_labels.npy", labels_val)
+
+    print("Test: ", len(test_names), labels_test.shape)
+    np.save("test_filenames.npy", test_names)
+    np.save("test_label.npy", labels_test)
